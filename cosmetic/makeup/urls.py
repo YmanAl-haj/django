b4bdcfd5b81ca3aa8c560c26cfb4d8a9c9c84ef6
YmanAl-haj <97166571+YmanAl-haj@users.noreply.github.com>
2022-01-31 08:50:08 +0300

    Update urls.py

--- cosmetic/makeup/urls.py
+++ cosmetic/makeup/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,re_path
-from makeup.views import index,brand,product,prodetails,branddetails
-from django.conf.urls.static import static
-from django.conf import settings
+from makeup.views import index,brand,product,prodetails,branddetails,updateItem,cart
+
 urlpatterns=[
-    path('index',index,name='index'),
+    path('',index,name='index'),
     path('brand',brand,name='brand'),
     path('product',product,name='product'),
     path('prodetails/<int:id>',prodetails,name='prodetails'),
     path('branddetails/<str:name>',branddetails,name='branddetails'),
+    path('updateItem/',updateItem,name='updateItem'),
+    path('cart/',cart,name='cart'),
 
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
